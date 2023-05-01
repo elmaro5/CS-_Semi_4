d1fdb3fc79216251dd22f8d7597c45c2c80b945f
fiji <kremus13@gmail.com>
2023-05-01 20:33:03 +0300

    Task_29

---
+++ Task_29.cs
@@ -0,0 +1,37 @@
+
+using System;
+
+int num = inputNumber(("Введите число: "));
+
+Console.WriteLine("[" + string.Join( ", ", createArray(num)) + "]");
+
+int[] createArray(int num)
+{
+    Random rand = new Random();
+    int[] array = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = rand.Next(0,9);
+    }
+    return array;
+}
+
+
+int inputNumber(string message)
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write(message);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
